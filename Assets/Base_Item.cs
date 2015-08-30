a4e16a6cf5be874dcf7d7e5ab4ff1b4c45206de3
Lawson Lamb <lawlzlamb@gmail.com>
2015-08-30 18:38:30 -0400

    xml test files with base_item class

---
+++ Assets/Base_Item.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum ItemType
+{
+    Weapon,Armor
+}
+public enum ActionType
+{
+   Damage,Healing,Curreny,Experiance
+
+}
+public class Base_Item { 
+    string n;
+    ItemType i_Type;
+    ActionType a_Type;
+    Sprite image;        
+   public Base_Item(string name)
+    {
+        n = name;
+       
+    }
+
+    public Base_Item(string name,ItemType item_Type,ActionType action_Type,float actionAmount)
+    {
+        
+        n = name;
+        i_Type = item_Type;
+        a_Type = action_Type;
+        
+    }
+
+    public string Name
+    {
+
+        get
+        {
+            return n;
+        }
+    }
+
+    public string ActionType_To_String
+    {
+
+        get
+        {
+            return a_Type.ToString();
+        }
+    }
+
+    public string ItemType_To_String
+    {
+        get
+        {
+            return i_Type.ToString();
+        }
+    }
+
+    public void Print_Stats()
+    {
+        
+        Debug.Log(Name + "," + ItemType_To_String+" ," +ActionType_To_String + " ,");
+    }
+   
+ 
+   
+
+}
---
+++ Assets/Test_Items.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Linq;
+using System.Linq;
+
+public class Item
+{
+    public string name { get; set; }
+    public string itemtype { get; set; }
+    public string actionType { get; set; }
+    public float actionValue { get; set; }
+}
+public class Test_Items : MonoBehaviour {
+   public Base_Item i1;
+    public Base_Item i2;
+	// Use this for initialization
+	void Start () {
+        i1 = new Base_Item("item");
+        i2 = new Base_Item("weapon", ItemType.Weapon, ActionType.Damage, 1.0f);
+        //i1.Print_Stats();
+        // i2.Print_Stats();
+       
+       //XDocument doc = XDocument.Load("XMLFile1.xml");
+        XElement doc= XElement.Load("XMLFile1.xml");
+
+        var i = (from item in doc.Descendants("item")
+                 select new Item
+                 {
+                     name = item.Element("name").Value,
+                     itemtype = item.Element("ItemType").Value,
+                     actionType = item.Element("ActionType").Value,
+
+
+                 }).ToList();
+        foreach( var item in i)
+        {
+
+            print(item.name);
+            print(item.itemtype);
+            print(item.actionType);
+           
+        }
+
+
+/*
+        var items = doc.Descendants("item");
+       
+       
+        foreach (var item in items)
+        {
+                
+              
+         
+        }
+    */
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    
+       
+	}
+}
